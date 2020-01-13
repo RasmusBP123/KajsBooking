7539cfb4275a1d19a85bbe217d58207f0d40ef0a
Rasmus Bak Petersen <rasm105k@edu.eal.dk>
2020-01-13 11:10:30 +0100

    Add project files.

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+
+        }
+
+
+    }
+}
